67ed84d419effbed0472f7ac8857ed9d03e4218b
Duvan Vasquez <32588639+duvan-vasquez@users.noreply.github.com>
2023-10-03 01:07:13 +0000

    Update CanvasCamera.java

--- src/android/CanvasCamera.java
+++ src/android/CanvasCamera.java
@@ -63,7 +63,7 @@ public class CanvasCamera extends CordovaPlugin implements CanvasCameraInterface
     private static final int SEC_CAMERA_POSITION = 3;
 
     private final static String[] FILENAMES = {"fullsize", "thumbnail"};
-    private final static String[] PERMISSIONS = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private final static String[] PERMISSIONS = {Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.READ_MEDIA_VIDEO, Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     protected int mFps;
     protected int mWidth;
@@ -476,7 +476,7 @@ public class CanvasCamera extends CordovaPlugin implements CanvasCameraInterface
         callbackContext.sendPluginResult(pluginResult);
     }
 
-    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+    public void onRequestPermissionResult(int requestCode, String[] , int[] grantResults) throws JSONException {
         for (int r : grantResults) {
             if (r == PackageManager.PERMISSION_DENIED) {
                 if (LOGGING) Log.w(TAG, "Permission Denied !");
